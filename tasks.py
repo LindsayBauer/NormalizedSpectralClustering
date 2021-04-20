0ca061b7129da6c5b9b755a9d1edc4ae11f16ef8
LindsayBauer <47898798+LindsayBauer@users.noreply.github.com>
2021-04-20 19:37:58 +0300

    Remove comment

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ from invoke import task
 
 @task
 def run(c, k=-1, n=-1, Random=True):
-    #c.run("python3.8.5 setup.py build_ext --inplace")
+    c.run("python3.8.5 setup.py build_ext --inplace")
     if not Random:
         bool_type = 0
         c.run(f"python3.8 main.py {k} {n} {bool_type}")
