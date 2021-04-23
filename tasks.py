713f3fc635333094ee8337d1e60a26af4ffa13e9
danaonn <63550185+danaonn@users.noreply.github.com>
2021-04-23 21:21:23 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -5,8 +5,8 @@ def run(c, k=-1, n=-1, Random=True):
     c.run("python3.8.5 setup.py build_ext --inplace")
     if not Random:
         bool_type = 0
-        c.run(f"python3.8 main.py {k} {n} {bool_type}")
+        c.run(f"python3.8.5 main.py {k} {n} {bool_type}")
     else:
         bool_type = 1
-        c.run(f"python3.8 main.py {k} {n} {bool_type}")
+        c.run(f"python3.8.5 main.py {k} {n} {bool_type}")
     print("Done building")
